deba3197800bec35effb80872863e8bd4850034e
Thom May <tmay@expedia.com>
2014-03-17 16:39:24 +0000

    Detect and raise errors from the API

--- lib/scalr/client.rb
+++ lib/scalr/client.rb
@@ -5,6 +5,28 @@ require 'base64'
 require 'time'
 
 class Scalr
+
+  class ResponseError < Faraday::Error::ClientError
+    attr_reader :response
+
+    def initialize(response)
+      super response["Error"]["Message"]
+    end
+  end
+
+  class APIError < Faraday::Response::Middleware
+    dependency 'multi_xml'
+
+    def call(env)
+      @app.call(env).on_complete do |e|
+        body = ::MultiXml.parse(e[:body])
+        if body["Error"]
+          raise Scalr::ResponseError, body
+        end
+      end
+    end
+  end
+
   class Client
     attr_accessor :key, :secret, :url, :env
 
@@ -16,31 +38,34 @@ class Scalr
     end
 
     def client
-      @client ||= Faraday.new(url: @url, ssl: {verify: false, version: 'SSLv3'}) do |f|
+      @client ||= Faraday.new(url: @url,
+                              ssl: { verify: false, version: 'SSLv3' }) do |f|
         f.request   :url_encoded
-        f.response  :json, :content_type => /\bjson$/
         f.response  :xml, content_type: /\bxml$/
         f.response  :logger
+        f.use       Scalr::APIError
         f.adapter   Faraday.default_adapter
       end
     end
 
-    def get(action, params={})
-      action(action, :get, params)
+    def get(action, params = {})
+      action(action, :get, params).body
     end
 
     private
+
     def timestamp
       Time.now.getgm.iso8601
     end
 
     def sig(action, timestamp)
       string = "#{action}:#{@key}:#{timestamp}"
-      mac = OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new("sha256"), @secret, string)
+      mac = OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('sha256'),
+                                 @secret, string)
       Base64.strict_encode64(mac)
     end
 
-    def action(action, mode=:get, params={})
+    def action(action, mode = :get, params = {})
       time = timestamp
       additional = { Signature: sig(action, time),
                      Action: action,
@@ -50,9 +75,8 @@ class Scalr
                      AuthVersion: 3,
                      EnvID: @env
       }
-      p = params.merge(additional).delete_if { |k,v| v.nil? }
+      p = params.merge(additional).delete_if { |k, v| v.nil? }
       client.send(mode, '/api/api.php', p)
     end
-
   end
 end
